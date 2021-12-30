bb3d5662e766264e4499470a8d7afbeae069f57a
Ponomarev Ivan <ponomarew_ivan@mail.ru>
2021-12-30 11:37:08 +0400

    fix error

--- 1.2-requests-templates/pagination/stations/views.py
+++ 1.2-requests-templates/pagination/stations/views.py
@@ -3,20 +3,20 @@ from django.urls import reverse
 import csv
 from django.core.paginator import Paginator
 
+with open('data-398-2018-08-30.csv', "r", encoding="utf-8") as csvfile:
+        reader = csv.DictReader(csvfile)
+        bus_stops_list = [row for row in reader]
 
 def index(request):
     return redirect(reverse('bus_stations'))
 
 
 def bus_stations(request):
-    with open('data-398-2018-08-30.csv', "r", encoding="utf-8") as csvfile:
-        reader = csv.DictReader(csvfile)
-        bus_stops_list = [row for row in reader]
     page_number = int(request.GET.get("page", 1))
     paginator = Paginator(bus_stops_list, 10)
     page = paginator.get_page(page_number)
     context = {
-        'bus_stations': page,
+        'bus_stations': page.object_list,
         'page': page
     }
     return render(request, 'stations/index.html', context)
