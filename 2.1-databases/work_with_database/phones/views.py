26f78180aea126e7a728e79663068ee65f0e56cd
ivan <ivan_ponomarew@mail.ru>
2022-01-15 11:31:46 +0400

    fix error and added sort

--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -7,22 +7,33 @@ def index(request):
 
 
 def show_catalog(request):
-    sort = request.GET.get('name', 'id')
-    sort_values = {
-        'name': 'name',
-        'min_price': '-price',
-        'max_price': 'price',
-        'id': 'id',
-    }
-    phones = Phone.objects.order_by(sort_values[sort]).all()
     template = 'catalog.html'
-    context = {'phones': phones,
-               'sort': sort, }
+    if 'sort' in request.GET:
+        sort_type = str(request.GET.get('sort'))
+        if sort_type == 'min_price':
+            sort_type = 'price'
+            context = {
+                'phones': Phone.objects.order_by(sort_type)
+            }
+            return render(request, template, context)
+        if sort_type == 'max_price':
+            sort_type = 'price'
+            context = {
+                'phones': reversed(Phone.objects.order_by(sort_type))
+            }
+            return render(request, template, context)
+        context = {
+            'phones': Phone.objects.order_by(sort_type)
+        }
+        return render(request, template, context)
+    context = {
+        'phones': Phone.objects.all()
+    }
     return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
-    phone = Phone.objects.filter(slug=slug).get()
+    phone = Phone.objects.get(slug=slug)
     context = {'phone': phone, }
     return render(request, template, context)
