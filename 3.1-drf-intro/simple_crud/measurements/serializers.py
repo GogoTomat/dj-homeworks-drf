6290ebac35fc3573cb5046c0c5ad2d930e81136e
ivan <ivan_ponomarew@mail.ru>
2022-02-01 22:31:49 +0400

    task 3.1

--- 3.1-drf-intro/simple_crud/measurements/serializers.py
+++ 3.1-drf-intro/simple_crud/measurements/serializers.py
@@ -1,1 +1,21 @@
-# TODO: опишите сериализаторы
+from rest_framework import serializers
+from .models import *
+
+
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Project
+        fields = '__all__'
+
+
+class MeasurementSerializer(serializers.ModelSerializer):
+
+    screen = serializers.ImageField(max_length=None,
+                                    use_url=True,
+                                    allow_null=True,
+                                    required=False
+                                    )
+
+    class Meta:
+        model = Measurement
+        fields = '__all__'
--- 3.1-drf-intro/simple_crud/simple_crud/urls.py
+++ 3.1-drf-intro/simple_crud/simple_crud/urls.py
@@ -14,10 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from measurements.views import ProjectViewSet, MeasurementViewSet
 
-# TODO: настройте роутер и подключите `ProjectViewSet` и `MeasurementViewSet`
+router = DefaultRouter()
+router.register('project', ProjectViewSet)
+router.register('measurement', MeasurementViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/', include(router.urls)),
 ]
