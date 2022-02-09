63d4dc678d4eb4eed2d937ffa37db20f712e89fb
ivan <ivan_ponomarew@mail.ru>
2022-02-09 17:14:05 +0400

    CRUD

--- 3.2-crud/stocks_products/logistic/views.py
+++ 3.2-crud/stocks_products/logistic/views.py
@@ -9,7 +9,7 @@ class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     filter_backends = [SearchFilter]
-    filterset_fields = ['title', 'description']
+    search_fields = ['title', 'description']
 
 
 class StockViewSet(ModelViewSet):
